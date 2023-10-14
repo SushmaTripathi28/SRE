2811af36b42730d781b9b253edba086ba41a3dfd
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2023-10-14 15:22:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
+project_id = "united-catfish-395610"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -16,3 +17,5 @@ resource "google_compute_instance" "default" {
     }
   }
 }
+
+
