e0a2883b947ca41e37eb3c203500bccba70bda58
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2023-10-14 15:25:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "default" {
+/*resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
@@ -18,4 +18,4 @@ project = "united-catfish-395610"
   }
 }
 
-
+*/
