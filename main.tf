6ebbc565cef76efc92bbd9eaf9e2ffa6e7378170
SushmaTripathi28 <128452108+SushmaTripathi28@users.noreply.github.com>
2024-02-08 20:02:47 +0530

    Update main.tf
    ALERT POLICY2

--- main.tf
+++ main.tf
@@ -1,20 +1,32 @@
 resource "google_monitoring_alert_policy" "alert_policy" {
-  display_name = "My Alert Policy s"
+  display_name = "CPU Utilization > 50%"
+  documentation {
+    content = "The $${metric.display_name} of the $${resource.type} $${resource.label.instance_id} in $${resource.project} has exceeded 50% for over 1 minute."
+  }
   combiner     = "OR"
   conditions {
-    display_name = "test condition"
+    display_name = "Condition 1"
     condition_threshold {
-      filter     = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
-      duration   = "60s"
-      comparison = "COMPARISON_GT"
-      aggregations {
-        alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
-      }
+        comparison = "COMPARISON_GT"
+        duration = "60s"
+        filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
+        threshold_value = "0.5"
+        trigger {
+          count = "1"
+        }
     }
   }
 
+  alert_strategy {
+    notification_channel_strategy {
+        renotify_interval = "1800s"
+        notification_channel_names = [google_monitoring_notification_channel.email.name]
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.name]
+
   user_labels = {
-    foo = "bar"
+    severity = "warning"
   }
 }
